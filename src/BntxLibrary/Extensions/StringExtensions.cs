2e412045504aaaba93306ecac401cbc9d1544b6b
ArchLeaders <archleadership28@gmail.com>
2024-02-09 01:27:56 -0800

    Create strings reader extensions

---
+++ src/BntxLibrary/Extensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Revrs.Extensions;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BntxLibrary.Extensions;
+
+public static class StringExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Span<byte> ReadPascalSting(this Span<byte> data)
+    {
+        ref ushort length = ref data.Read<ushort>();
+        return data[sizeof(ushort)..(sizeof(ushort) + length)];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string ToManaged(this Span<byte> data)
+    {
+        return Encoding.UTF8.GetString(data);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string ToManaged(this ReadOnlySpan<byte> data)
+    {
+        return Encoding.UTF8.GetString(data);
+    }
+}
