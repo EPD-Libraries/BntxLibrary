7b0caa32ac4d1ac68cda33115e914fb2fd556927
ArchLeaders <archleadership28@gmail.com>
2024-02-10 16:37:28 -0800

    Impl block writer

--- src/BntxLibrary/Structures/Common/BinaryBlockHeader.cs
+++ src/BntxLibrary/Structures/Common/BinaryBlockHeader.cs
@@ -1,4 +1,5 @@
-﻿using Revrs;
+﻿using BntxLibrary.Writers.WriterContextModels;
+using Revrs;
 
 namespace BntxLibrary.Structures.Common;
 
@@ -8,6 +9,23 @@ public struct BinaryBlockHeader
     public uint NextBlockOffset;
     public uint BlockSize;
     public uint Reserved;
+
+    public static long Write<T>(in T context, uint magic, Action<T> write) where T : IWriterContext
+    {
+        long headerOffset = context.Writer.Position;
+        write(context);
+        context.Writer.Align(0x8);
+        uint blockSize = Convert.ToUInt32(context.Writer.Position - headerOffset);
+
+        context.Writer.Seek(headerOffset);
+        context.Writer.Write(new BinaryBlockHeader {
+            Magic = magic,
+            NextBlockOffset = blockSize,
+            BlockSize = blockSize
+        });
+
+        return headerOffset;
+    }
 
     public class Reverser : IStructReverser
     {
